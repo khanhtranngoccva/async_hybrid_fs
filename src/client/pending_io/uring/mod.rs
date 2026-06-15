15d910076643d9e047a2f34f3b82a01670663fda
Khanh Tran <khanhtranngoccva@gmail.com>
2026-06-15 14:51:27 +0700

    fix: change order to drop ticket outside lock

--- src/client/pending_io/uring/mod.rs
+++ src/client/pending_io/uring/mod.rs
@@ -266,13 +266,14 @@ impl UringPendingIoFiller {
         state.status = UringPendingIoStatus::Done;
         state.result = Some(result);
         // Drop the ticket here so that the tickets can be freed even on a blocked runtime.
-        drop(state.submission_ticket.take());
+        let ticket = state.submission_ticket.take();
         // We would like to remove the ticket, but it blocks the reaper thread.
         // Notify the future or the blocking thread that the operation changed its state.
         let waker = core::mem::replace(&mut state.waker, Waker::noop().clone());
         drop(state);
         waker.wake();
         self.transition_cv.notify_one();
+        drop(ticket);
     }
 }
 
